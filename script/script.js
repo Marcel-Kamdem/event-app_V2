3d7079073ff0a36f347ebb0bfbada44e14c0060c
Jonh-jonhy <jonathankamdem000@gmail.com>
2026-01-12 23:10:56 +0100

    debut du projet Page visiteur header, first section, categorie

---
+++ script/script.js
@@ -0,0 +1,42 @@
+var ham = document.querySelector(".ham");
+var hamLight = document.querySelector(".light-ham");
+var hamDark = document.querySelector(".dark-ham");
+var offScreen = document.querySelector(".off-screen-menu");
+var body = document.querySelector("body");
+var overlay = document.querySelector(".overlay");
+var themeButtun = document.querySelector(".theme-btn");
+var moon = document.querySelector(".moon");
+var sun = document.querySelector(".sun");
+/*
+*  Overlay humberger button
+**/
+ham.addEventListener("click", function () {
+    offScreen.classList.add('on');
+    overlay.style.display = "block";
+});
+//fermer le off screen menu lorsque l'on clique hors de la div
+overlay.addEventListener("click", function () {
+    offScreen.classList.remove('on');
+    overlay.style.display = "none";
+});
+/*
+*  switching theme
+**/
+sun.addEventListener("click", function () {
+    body.classList.add("theme-light");
+    sun.style.display = "none";
+    moon.style.display = "block";
+    if (window.innerWidth < 500) {
+        hamLight.style.display = "none";
+        hamDark.style.display = "block";
+    }
+});
+moon.addEventListener("click", function () {
+    body.classList.remove("theme-light");
+    sun.style.display = "block";
+    moon.style.display = "none";
+    if (window.innerWidth < 500) {
+        hamLight.style.display = "block";
+        hamDark.style.display = "none";
+    }
+});
---
+++ script/script.ts
@@ -0,0 +1,46 @@
+const ham = document.querySelector(".ham") as HTMLElement;
+const hamLight = document.querySelector(".light-ham") as HTMLElement;
+const hamDark = document.querySelector(".dark-ham") as HTMLElement;
+const offScreen = document.querySelector(".off-screen-menu") as HTMLElement;
+const body = document.querySelector("body") as HTMLElement;
+const overlay = document.querySelector(".overlay") as HTMLElement;
+const themeButtun = document.querySelector(".theme-btn") as HTMLElement;
+const moon = document.querySelector(".moon") as HTMLElement;
+const sun = document.querySelector(".sun") as HTMLElement;
+
+/*
+*  Overlay humberger button
+**/ 
+ham.addEventListener("click",()=>{
+    offScreen.classList.add('on');
+    overlay.style.display = "block";
+} );
+
+//fermer le off screen menu lorsque l'on clique hors de la div
+overlay.addEventListener("click",()=>{
+        offScreen.classList.remove('on');
+        overlay.style.display = "none";
+});
+
+/*
+*  switching theme
+**/ 
+sun.addEventListener("click", ()=>{
+    body.classList.add("theme-light");
+    sun.style.display = "none";
+    moon.style.display = "block";
+    if(window.innerWidth < 500){
+        hamLight.style.display = "none";
+        hamDark.style.display= "block";
+    }
+})
+
+moon.addEventListener("click", ()=>{
+    body.classList.remove("theme-light");
+    sun.style.display = "block";
+    moon.style.display = "none";
+    if (window.innerWidth < 500) {
+        hamLight.style.display = "block";
+        hamDark.style.display= "none";
+    }
+})
